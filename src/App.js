e40e1209787f61a4ac36fc82f51b7a0c7b163620
“Oscar <oscarparedes.uc@gmail.com>
2023-09-13 22:13:35 -0500

    hasta el cap 23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const estilo2 = {
+  boxShadow: '0 5px 3px rgba(0,0,0,1)'
 }
+
+const estilo = (bg = '#222') => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px'
+})
+
+const Li = ({ children }) => {
+  console.log(children)
+  return (
+    <li style={{ ...estilo2, ...estilo({ bg: '#333' }) }} className='clase-li'>{children}</li>
+  )
+};
+
+const App = () => {
+  //const valor = 'triste'
+  return (
+    // jsx como html pero en javascript
+    <ul style={estilo({ bg: '#750' })} className='clase-css'>
+      {/* Chanchito {valor} */}
+      <Li estado="feliz" >valor de Li</Li>
+    </ul>
+
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//forma moderna
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
     <App />
   </React.StrictMode>
-);
+ );
+
+//forma antigua
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+//clase 21 inicial
+
+// <p>Hola mundo</p>
+//const x = React.createElement('p', null, 'Hola mundo')
+//const X = () => <p>Hola mundo</p> //componente funcional que retorna jsx
+//componente estático
+// const X = () => <ul>
+//   <li>Chancito Feliz</li>
+//   <li>Chancito Triste</li>
+//   <li>Felipe</li>
+// </ul>
+
+//clase 21 inicial fin
+
+//componente final clase 21
+
+// const Li = ({ children, estado, casa, edad }) => {
+//   console.log(casa, edad);
+//   return(<li>{children} estoy {estado}</li>)
+// }
+
+// const X = () => <ul>
+//   <Li
+//     estado={'feliz'}
+//     casa={false}
+//     edad={24}
+//   >Chanchito</Li>
+//   <Li estado={'triste'}>Chanchito</Li>
+//   <Li estado={'emocionado'}>Felipe</Li>
+// </ul>
+
+//fin componente final clase 21
+
+// ReactDOM.render : Encargado de renderizar una aplicación
+// se debe cambiar el paquete import ReactDOM from 'react-dom/client';
+// a import ReactDOM from 'react-dom´; y se usa en vez del createElement
+// ReactDOM.render(
+//   <X />, document.getElementById('root')
+//   //jsx
+//   //<p>Hola mundo</p>, document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
